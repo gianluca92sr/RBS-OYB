3bd823eab2204eddf2686741b6d6ee02ba315d4d
PCFISSO\gianluca <gianluk1992sr@gmail.com>
2021-11-09 02:01:40 +0100

    Removed hashtag from tweet text

--- Twitter.py
+++ Twitter.py
@@ -8,6 +8,7 @@ import pandas as pd
 import re
 import nltk;
 from PIL import Image
+
 nltk.download('stopwords')
 from pprint import pprint
 import numpy as np
@@ -61,6 +62,7 @@ token = WordPunctTokenizer()
 # Remove any special characters and http link, finally convert all in lowercase
 def cleaning_tweets(t):
     pattern_mentions = '@[A-Za-z0–9_]+'
+    pattern_hashtag = '#(\w+)'
     link_specialchar = [
         r'(https?://)?(www\.)?(\w+\.)?(\w+)(\.\w+)(/.+)?',
         '[^A-Za-z0-9]+'
@@ -69,16 +71,17 @@ def cleaning_tweets(t):
     del_amp = BeautifulSoup(t, 'lxml')
     del_amp_text = del_amp.get_text()
     text_tweet_no_mentions = re.sub(pattern_mentions, ' ', del_amp_text)
-    text_tweet_no_link_spec_char = re.sub(pattern_link_special_char, ' ', text_tweet_no_mentions)
+    text_tweet_no_hashtag = re.sub(pattern_hashtag, ' ', text_tweet_no_mentions)
+    text_tweet_no_link_spec_char = re.sub(pattern_link_special_char, ' ', text_tweet_no_hashtag)
     lower_case = text_tweet_no_link_spec_char.lower()
     words = token.tokenize(lower_case)
     result_words = [x for x in words if len(x) > 2]
     return (" ".join(result_words)).strip()
 
+
 def get_hashtag(t):
     lower_case = t.lower()
     return re.findall('#(\w+)', lower_case)
-
 
 
 tweets_text = []
@@ -98,7 +101,8 @@ for i in range(len(tweetsFull)):
 
 # DATA FORMATTING
 df = pd.DataFrame(list(
-    zip(tweets_text, tweets_replies, tweets_retweets, tweets_likes, (pd.to_datetime(tweets_time, format='%H:%M:%S')).time)),
+    zip(tweets_text, tweets_replies, tweets_retweets, tweets_likes,
+        (pd.to_datetime(tweets_time, format='%H:%M:%S')).time)),
     columns=['Tweet', 'Replies_Count', 'Retweets_Count', 'Likes_Count', 'Timestamp'])
 
 tweetsString = pd.Series(df.Tweet.values).str.cat(sep=' ')
@@ -107,8 +111,9 @@ stopwords.update(["blockchain", "eth", "bnb", "btc", "food", "int", "one", "xdc"
 wordcloud = WordCloud(width=1600, stopwords=stopwords, height=800, max_font_size=200, max_words=50, collocations=False,
                       background_color='white').generate(tweetsString)
 
-wordcloud_hash = WordCloud(width=1600, stopwords=stopwords, height=800, max_font_size=200, max_words=50, collocations=False,
-                      background_color='black').generate(" ".join(tweets_hashtag).strip())
+wordcloud_hash = WordCloud(width=1600, stopwords=stopwords, height=800, max_font_size=200, max_words=50,
+                           collocations=False,
+                           background_color='black').generate(" ".join(tweets_hashtag).strip())
 
 plt.figure(figsize=(40, 30))
 plt.imshow(wordcloud, interpolation='bilinear')
